5654cbdb3d7198e9f374d5cc949d1a211cb14f38
ryanmgreen96 <115604086+ryanmgreen96@users.noreply.github.com>
2025-05-28 14:01:20 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,39 @@
-const CACHE_NAME = 'pwa-cache-v1';
+const CACHE_NAME = 'my-pwa-cache-v1';
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
-  '/style.css',
-  '/script.js',
   '/jquery.min.js',
+  '/script.js',
+  '/style.css',
   '/register1.html',
-  '/manifest.json',
-  '/icon.png'
+  '/register2.html',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', (evt) => {
   evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (evt) => {
   evt.respondWith(
-    caches.match(evt.request).then((response) => response || fetch(evt.request))
+    caches.match(evt.request).then(response => {
+      return response || fetch(evt.request);
+    })
   );
 });
